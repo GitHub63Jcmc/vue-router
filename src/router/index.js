794c03d98593dd8db7d8b238ceb1ae9044a75d7b
Joao Cabral de Matos Carvalho <jcmcgoojcmc@gmail.com>
2025-06-11 19:07:29 +0200

    32 - Rutas con parámetros.

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,10 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: '/courses/:courseId',
+      component: () => import('../views/CouseShowView.vue'),
+    },
+    {
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue'),
---
+++ src/views/CouseShowView.vue
@@ -0,0 +1,20 @@
+<script setup>
+    import { useRoute } from 'vue-router'
+    import { computed } from 'vue';
+
+    const route = useRoute();
+    const courseId = computed(() => route.params.courseId);
+
+    console.log(route.params.courseId)
+</script>
+
+<template>
+  <div class="about">
+    <!-- <h1>Bienvenido al curso: {{ $route.params.courseId }}</h1> -->
+    <h1>Bienvenido al curso: {{ courseId }}</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
